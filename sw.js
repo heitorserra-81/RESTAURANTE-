82e93b17ec234aa7a927a366fcf4fc0241c5a6ff
heitorserra-81 <heitorserraneto@gmail.com>
2026-07-25 09:54:47 -0300

    Update cache name from refeitorio-v10 to refeitorio-v12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NOME = 'refeitorio-v10';
+const CACHE_NOME = 'refeitorio-v12';
 const ARQUIVOS_BASE = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', (evento) => {
