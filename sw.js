f177e4b20d8b768ea2c92ee65a7059b7a386be56
heitorserra-81 <heitorserraneto@gmail.com>
2026-07-27 15:25:41 -0300

    Update cache name from 'refeitorio-v14' to 'refeitorio-v15'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NOME = 'refeitorio-v14';
+const CACHE_NOME = 'refeitorio-v15';
 const ARQUIVOS_BASE = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', (evento) => {
